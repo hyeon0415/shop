57b8f405a69d103f197b076b28914a55f5c90d59
hyeon0415 <82595578+hyeon0415@users.noreply.github.com>
2023-04-02 14:54:44 +0900

    2023. 04. 02
    nested routes

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import {Button, Navbar, Container, Nav} from 'react-bootstrap';
 import './App.css';
 import bg from './img/bg.png';
 import data from './data.js';
-import { Routes, Route, Link } from 'react-router-dom' 
+import { Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom';
+import Detail from './routes/Detail';
 
 function App() {
 
   let [shoes] = useState(data)
-  console.log(shoes[0].price);
+  let navigate = useNavigate(); // 페이지 이동을 도와주는 함수
   return (
     <div className="App">
 
@@ -16,14 +17,14 @@ function App() {
         <Container>
           <Navbar.Brand href="#home">ShopShop</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Cart</Nav.Link>
+            <Nav.Link onClick={()=>{ navigate('/')}}>Home</Nav.Link>
+            <Nav.Link onClick={()=>{ navigate('/detail') }}>Detail</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
 
-      <Link to="/">홈</Link>
-      <Link to="/detail">상세페이지</Link>
+      {/* <Link to="/">홈</Link> */}
+      {/* <Link to="/detail">상세페이지</Link> */}
 
       <Routes>
         <Route path="/" element={
@@ -45,11 +46,39 @@ function App() {
                 </div>
           </>      
         }/>
-        <Route path="/detail" element={<div>상세페이지임</div>}/>
+        <Route path="/detail" element={<Detail/>}/>
+        <Route path="/about" element={<About/>}>
+        <Route path="member" element={<div>멤버임</div>}/>
+        <Route path="location" element={<div>위치정보임</div>}/>
+        </Route>
+
+        <Route path="/event" element={<EventPage/>}>
+        <Route path="one" element={<div>첫 주문시 양배추즙 서비스</div>}/>
+        <Route path="two" element={<div>생일기념 쿠폰받기</div>}/>
+        </Route>
+
+        {/* 그 이외의 페이지(404표시를 위한)<Route path="*" element={<div>없는페이지요</div>}/>  */}
       </Routes>
 
     </div>
   );
+}
+function About(){
+  return (
+    <div>
+      <h4>회사정보임</h4>
+      <Outlet></Outlet>
+    </div>
+  )
+}
+
+function EventPage(){
+  return (
+    <div>
+      <h4>오늘의 이벤트</h4>
+      <Outlet></Outlet>
+    </div>
+  )
 }
 
 function Card(props){
