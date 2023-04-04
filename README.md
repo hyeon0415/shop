c11feaaecb0ea111b0beb28a353a1dd938c91d00
hyeon0415 <82595578+hyeon0415@users.noreply.github.com>
2023-04-04 21:49:00 +0900

    2023. 04. 04
    styled-components

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 ## npm install react-router-dom@6 
 리액트 라우더 다운 명령어
+
+## npm install styled-components
+리액트 styled-components 다운 명령어
--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,13 +1,27 @@
 import { useParams } from "react-router-dom";
+import styled from 'styled-components'
+
+// css 파일 안가도됨
+let YellowBtn = styled.button`
+    background : ${props => props.bg};
+    color : ${props => props.bg == 'blue' ? 'white' : 'black'};
+    padding : 10px;
+`
+
+let Box = styled.div`
+    background : grey;
+    paddinf : 20px;
+`
+
 
 function Detail(props){
     
     // 유저가 :id에 파라미터 입력한 겂을 가져옴
     let {id} = useParams();
-    console.log(id);
-
     return (
         <div className="container">
+            <YellowBtn bg="blue">버튼</YellowBtn>
+            <YellowBtn bg="orange">버튼</YellowBtn>
             <div className="row">
                 <div className="col-md-6">
                     <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
