d2c24976b5d53e04209cbebc32c18fb335590d24
hyeon0415 <82595578+hyeon0415@users.noreply.github.com>
2023-04-05 22:07:50 +0900

    2023. 04. 05
    useEffect

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,5 +1,7 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import styled from 'styled-components'
+import styled from 'styled-components';
+
 
 // css 파일 안가도됨
 let YellowBtn = styled.button`
@@ -16,10 +18,27 @@ let Box = styled.div`
 
 function Detail(props){
     
+    // html 렌더링 후에 동작
+    // 어려운 연산작업들, 서버에서 데이터 가져오는 작업, 타이머 장착하는거
+    useEffect(()=>{
+        // for (var i = 0; i < 10000; i++){
+        //     console.log(i);
+        // }   
+    })
+
+    setTimeout(()=>{ }, 2);
+   
+
+    let [count, setCount] = useState(0);
     // 유저가 :id에 파라미터 입력한 겂을 가져옴
     let {id} = useParams();
     return (
         <div className="container">
+            <div className="alert alert-warning">
+                2초이내 구매시 할인
+            </div>
+            {count}
+            <button onClick={()=> { setCount(count+1) }}>버튼</button>
             <YellowBtn bg="blue">버튼</YellowBtn>
             <YellowBtn bg="orange">버튼</YellowBtn>
             <div className="row">
